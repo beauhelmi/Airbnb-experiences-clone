5926601d75f95d82233fbb4484a89160947fcc0d
beauhelmi <beauhelmi@gmail.com>
2022-11-01 22:30:39 +0800

    done first part of AirBnb info site, dealing with Navbar and Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import heroicon from "../images/heroicon.png";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <img src={heroicon} className="hero--icon"/>
+      <h1 className="hero--title">Online experiences</h1>
+      <p className="hero--text">
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
+    </section>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import logo from "../images/airbnb.png"
+
+export default function Navbar() {
+    return(
+        <nav>
+            <img src={logo} className="logo--icon"/>
+        </nav>
+    )
+}
