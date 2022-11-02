70de26b0bf78ae7db0c2c5a56b9a1a22649e0325
beauhelmi <beauhelmi@gmail.com>
2022-11-02 11:04:36 +0800

    add styling to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,11 +7,11 @@ export default function Card(){
             <div className="card--stats">
                 <img src={star} className="card--star" />
                 <span>5.0</span>
-                <span>(6) • </span>
-                <span>USA</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
             </div>
             <p>Life Lessons with Katie Zaferes</p>
-            <p><span>From $136</span> / person</p>
+            <p><span className="bold">From $136</span>/ person</p>
         </div>
     )
 }
