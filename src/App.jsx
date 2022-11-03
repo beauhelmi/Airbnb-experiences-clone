402f33234531622ffe1a83cb0e00bbde9aa68b01
beauhelmi <beauhelmi@gmail.com>
2022-11-03 16:12:16 +0800

    manage to insert img and pass as props

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import './App.css'
 import Navbar from "./components/Navbar"
 import Hero from './components/Hero'
 import Card from './components/Card'
+import myImg from './images/katiezaferes.png'
 
 export default function App() {
   return(
     <div>
     <Navbar />
     <Hero />
-    <Card />
+    <Card
+    img={myImg}
+    rating="5.0"
+    reviewCount={6}
+    country="MALAYSIA"
+    title="Life Lessons with Katie Zaferes"
+    price={136}
+    />
     </div>
     )
   }
